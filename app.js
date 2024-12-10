26b4f6d6e9f6c2df41be46e3ea023da8723e5e3a
rodrigo <rodrigo.gomes28599@al.aememmartins.pt>
2024-12-10 17:25:18 +0000

    inicio

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hello wordl');
+
+const express = require('express');
+const app =express();
+  app.get('/Carro',(req,res) => {
+    res.send('Seu Carro');
+
+  });
+  app.get('/Nome',(req,res) => {
+    res.send('teu Nome');
+});
+app.get('/Tarefas',(req,res) => {
+  res.status('404').send('nada');
+});
+app.get('/pagamentos',(req,res) => {
+  res.sendStatus('403');
+
+
+  });app.get('/Apelido',(req,res) => {
+    res.send('Teu Apelido ');
+
+  });
+  const PORT =3000;
+  app.listen(PORT,()=>{
+console.log(`server a correr em http:/localhost:${PORT}`);
+});
