ad74a0a4aa1450269a24f082df014469dfad951a
Prata <tiago.goncalves27323@al.aememmartins.pt>
2024-12-12 12:05:28 +0000

    ihvcjhfdc

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 
 const express = require('express');
+const path=require('path')
 const app =express();
   app.get('/Carro',(req,res) => {
-    res.send('Seu Carro');
+    res.sendFile(path.join(__dirname,'index.html'));
+
 
   });
-  app.get('/Nome',(req,res) => {
-    res.send('teu Nome');
+  app.get('/nome',(req,res) => {
+    res.sendFile(path.join(__dirname,'nome.html'));
+    
 });
 app.get('/Tarefas',(req,res) => {
   res.status('404').send('nada');
 });
 app.get('/pagamentos',(req,res) => {
-  res.sendStatus('403');
-
+  res.sendFile(path.join(__dirname,'pagamentos.html'));
 
   });app.get('/Apelido',(req,res) => {
     res.send('Teu Apelido ');
