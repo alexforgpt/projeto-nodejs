bf9b0ecb6cb4b8b68b4ace1b8269d310e474823c
rodrigo <rodrigo.gomes28599@al.aememmartins.pt>
2024-12-17 17:23:52 +0000

    isfndf

--- app.js
+++ app.js
@@ -1,27 +1,116 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+let moneyBalance = null;
+let numId = null;
+app.use(express.json());
+ 
+ 
+app.get('/', (req, res) => {
+  res.send('Página inicial')
+})
+app.get('/sobre', (req, res) => {
+    res.sendfile('Sobre mim')
+  })
+ 
+  app.get('/contacto', (req, res) => {
+    res.sendfile('Contactos:')
+  })
+ 
+  app.get('/tarefas', (req, res) => {
+    res.sendStatus(404)
+  })
 
-const express = require('express');
-const path=require('path')
-const app =express();
-  app.get('/Carro',(req,res) => {
-    res.sendFile(path.join(__dirname,'index.html'));
+  app.get('/pagamentos', (req, res) => {
+    res.status(403).send('Não econtrada')
+  })
+ 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+ 
+app.get('/livro', (req, res) =>{
+  const livro= req.body;
+  res.send(`O nome do livro é ${livro.nome} do artista ${livro.artista} no ano ${livro.ano}`)
+  })
 
-
-  });
-  app.get('/nome',(req,res) => {
-    res.sendFile(path.join(__dirname,'nome.html'));
+  app.get('/balance', (request, response) =>{
+ 
+    response.send(`${moneyBalance}`)
+  })
+  app.post('/balance', (request, response) =>{
+    if (moneyBalance == null){
+    moneyBalance = request.body.money;
+    response.sendStatus(200);
+  }
+    else
+    response.sendStatus(400);  
+  })   
+ app.put('/balance', (request, response) => {
+    if (moneyBalance == null) {
+        response.sendStatus(400);
+    }
+    if (moneyBalance != null) {
+        moneyBalance = request.body.money; 
+        response.sendStatus(200); 
+    }
+});
+  app.delete('/balance', (request, response) => {
+   
+    moneyBalance = null;
     
+    response.send(`${moneyBalance}`);
 });
-app.get('/Tarefas',(req,res) => {
-  res.status('404').send('nada');
-});
-app.get('/pagamentos',(req,res) => {
-  res.sendFile(path.join(__dirname,'pagamentos.html'));
+app.get('/', (req, res) => {
+  res.send('Página inicial')
+})
+app.get('/sobre', (req, res) => {
+    res.sendfile('Sobre mim')
+  })
+ 
+  app.get('/contacto', (req, res) => {
+    res.sendfile('Contactos:')
+  })
+ 
+  app.get('/tarefas', (req, res) => {
+    res.sendStatus(404)
+  })
+ 
+  app.get('/pagamentos', (req, res) => {
+    res.status(403).send('Não econtrada')
+  })
 
-  });app.get('/Apelido',(req,res) => {
-    res.send('Teu Apelido ');
+  app.get('/users', (req, res) => {
+    res.send(users);
+  })
 
-  });
-  const PORT =3000;
-  app.listen(PORT,()=>{
-console.log(`server a correr em http:/localhost:${PORT}`);
-});
+  app.post('/users', (req, res) => {
+    res.send(users);
+    
+  })
+  
+  app.put('/users', (req, res) => {
+    res.send(users);
+    
+  })
+
+  app.delete('/users', (req, res) => {
+    res.send(users);
+    
+  })
+  const users = [
+      {
+         id : 0,
+         first_name : 'John',
+         last_name : 'Doe',
+         email : 'johndoe@example.com'
+       },
+      
+       {
+       id : 1,
+       first_name : 'Alice',
+       last_name : 'Smith',
+       email : 'alicesmith@example.com'
+     }
+     ]
